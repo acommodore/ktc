d6838234f9f068a50c4cc600799cab9d2a0c985d
acommodore <ayeley.commodore@gmail.com>
2026-08-23 22:42:36 +0000

    Update Google Drive image URL format for better compatibility

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ function getGoogleDriveImageSrc(url) {
   if (!url) return '';
   const match = url.match(/id=([a-zA-Z0-9_-]+)/);
   if (match && match[1]) {
-    return `https://drive.google.com/uc?export=view&id=${match[1]}`;
+    return `https://drive.google.com/thumbnail?id=${match[1]}&sz=w1000`;
   }
   return url;
 }
